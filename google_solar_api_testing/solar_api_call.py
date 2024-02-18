db8b70548ae5bc67980cc4a619ef936843323683
Samuel Wolf <wolfs@kenyon.edu>
2024-02-19 08:27:07 +0900

    Remove layers function

--- google_solar_api_testing/solar_api_call.py
+++ google_solar_api_testing/solar_api_call.py
@@ -1,17 +1,9 @@
 import requests
 
-def data_layers(latitude, longitude, radius): # returns image and mask layer
-    url1 = f"https://solar.googleapis.com/v1/dataLayers:get?location.latitude={latitude}&location.longitude={longitude}&radiusMeters={radius}&view=FULL_LAYERS&requiredQuality=HIGH&pixelSizeMeters=0.5&key=AIzaSyBMq9USaxYUKJyC4YUC66xiu2si28UiiSI"
-    try:
-        response = requests.get(url1)
-        return response.json()
-    except:
-        return ''
-
 def building_insights(latitude, longitude): # returns insights
     url2 = f"https://solar.googleapis.com/v1/buildingInsights:findClosest?location.latitude={latitude}&location.longitude={longitude}&requiredQuality=HIGH&key=AIzaSyBMq9USaxYUKJyC4YUC66xiu2si28UiiSI"
     try:
-        response = requests.get(url2)
+        response = requests.get(url)
         return response.json()
     except:
         return ''
