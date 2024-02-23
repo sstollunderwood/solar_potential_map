6024b3bd8bd54d23c8add24e360927ec9057d997
Samuel Wolf <wolfs@kenyon.edu>
2024-02-23 23:02:26 +0900

    Move files to /api, delete /google_solar_api_testing

--- google_solar_api_testing/energy_generation.py
+++
@@ -1,4 +0,0 @@
-# based on x amount of area, how much energy can we generate
-
-def area_energy_generation(area, maxsunlighthours):
-    return area * maxsunlighthours
--- google_solar_api_testing/solar_api_call.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def building_insights(latitude, longitude): # returns insights
-    url2 = f"https://solar.googleapis.com/v1/buildingInsights:findClosest?location.latitude={latitude}&location.longitude={longitude}&requiredQuality=HIGH&key=AIzaSyBMq9USaxYUKJyC4YUC66xiu2si28UiiSI"
-    try:
-        response = requests.get(url)
-        return response.json()
-    except:
-        return ''
