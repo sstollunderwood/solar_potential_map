68cbf6517cbcd0799e9b4274b37d8556bfd9772b
Mark Jarrett <jarrettm101@gmail.com>
2024-02-23 16:07:37 +0900

    added more function, setting up env variables

---
+++ utils/params.py
@@ -0,0 +1,4 @@
+import os
+
+SOLAR_API_KEY = os.environ.get("SOLAR_API_KEY")
+MAPS_API_KEY = os.environ.get("MAPS_API_KEY")
--- utils/utils.py
+++ utils/utils.py
@@ -1,9 +1,14 @@
 import requests
 from PIL import Image
 import numpy as np
+from utils.params import *
 
-def watermark_remover(url):
-    #Takes the Google Maps API Image and returns it without the watermark
+def get_gmaps_image(lat,lon,zoom,size="572x594"):
+    #Returns Google Maps image with watermark removed
+    #Create Google Maps API call
+    url = f"https://maps.googleapis.com/maps/api/staticmap?center={lat},{lon}&zoom={zoom}&size={size}&maptype=satellite&key={MAPS_API_KEY}"
+
+    #Gets the Google Maps API Image and returns it without the watermark
     im = Image.open(requests.get(url,stream=True).raw)
     return im.crop((0,0,572,572))
 
@@ -12,7 +17,15 @@ def pixel_size_calculator(zoom,lat):
     #Based on the assumption earth's radius = 6378137m
 
     #Get pixel length
-    pixel_length = 156543.03392 * np.cos(lat * np.pi / 180) * (2 ** zoom)
+    pixel_length = 156543.03392 * np.cos(lat * np.pi / 180) / (2 ** zoom)
 
     #Return pixel area
     return pixel_length ** 2
+
+def building_insights(latitude, longitude): # returns insights
+    url = f"https://solar.googleapis.com/v1/buildingInsights:findClosest?location.latitude={latitude}&location.longitude={longitude}&requiredQuality=HIGH&key={SOLAR_API_KEY}"
+    try:
+        response = requests.get(url)
+        return response.json()
+    except:
+        return ''
