2df54e5d18f3cf54af8ec138b437fe4271180c01
Sarah Underwood <sstollunderwood@yahoo.com>
2024-03-02 13:13:30 +0900

    added parameter

--- utils/params.py
+++ utils/params.py
@@ -1,5 +1,6 @@
 import os
 
+API_RUN = os.environ.get("API_R")
 SOLAR_API_KEY = os.environ.get("SOLAR_API_KEY")
 MAPS_API_KEY = os.environ.get("MAPS_API_KEY")
 MODEL_BLOB_NAME = os.environ.get("MODEL_BLOB_NAME")
