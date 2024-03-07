61a69dcf930d18d91302325d6082a861b3224ff1
Sarah Underwood <sstollunderwood@yahoo.com>
2024-03-07 20:57:39 +0900

    formatting etc

--- app.py
+++ app.py
@@ -122,10 +122,9 @@ def main():
     with st.sidebar:
         st.subheader("How much solar power can the rooftops of an area generate?")
         st.divider()
-        st.write("Calculations for energy and co2 equivalents are based off of information provided by the United States EPA")
-        st.write("The average car released 4.20 metrics tons of CO2 a year")
-        st.write("The average home in the US consumed 12,154 kWh of electricity a year")
-
+        st.write("Calculations for energy and CO₂ equivalents are based off of information provided by the United States EPA.")
+        st.write("The average car releases 4.20 metric tons of CO₂ a year.")
+        st.write("The average home in the US consumes 12 megawatts of electricity a year.")
 
     #Establish column layout, details in left small column, map in larger right column
     col1, col2, col3= st.columns([4, 0.5, 5.5])
@@ -133,20 +132,22 @@ def main():
     with col1:
         st.write('')
         location = st.text_input("Enter location or address:", 'Le Wagon, Meguro, Tokyo')
-        zoom_level = st.slider("Zoom level:", 17, 20, 18)
+        zoom_level = st.slider("Zoom level:", 17, 20, 19)
 
     # Geocode location to get latitude and longitude
     geocode_result = gmaps.geocode(location)
     if geocode_result:
         lat = geocode_result[0]['geometry']['location']['lat']
         lng = geocode_result[0]['geometry']['location']['lng']
-        zipcode = geocode_result[0]['address_components'][0]['long_name']
-        neighborhood = geocode_result[0]['address_components'][3]['long_name']
-        city_name = geocode_result[0]['address_components'][4]['long_name']
-        prefecture_name = geocode_result[0]['address_components'][5]['long_name']
+        #zipcode = geocode_result[0]['address_components'][0]['long_name']
+        #neighborhood = geocode_result[0]['address_components'][3]['long_name']
+        if len(geocode_result[0]) > 6:
+            city_name = geocode_result[0]['address_components'][4]['long_name']
+        else:
+            city_name = geocode_result[0]['address_components'][1]['long_name']
 
         valid_cities = ["tokyo","osaka","nagoya","fukuoka","sapporo"]
-        if city_name not in valid_cities:
+        if city_name.lower().strip() not in valid_cities:
             city_name = 'tokyo'
         address = ""
         for component in geocode_result[0]['address_components']:
@@ -164,22 +165,22 @@ def main():
             if st.button("Calculate!", on_click=click_button):
                 #this is where the back end call will go
                 original_image = get_gmaps_image(lat, lng, zoom_level)
-                #mask_array = original_image
-                new_url = api_url+endpoint
-                request_post= send_backend(lat=lat, lng=lng, zoom=zoom_level, fast_url=new_url)
-                mask_json = request_post.json()
-                mask_array = np.array(mask_json['output_mask'])
-                mask = cv2.normalize(mask_array, dst=None, alpha=0,
-                                beta=255,norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
-                mask = smooth_image(mask, 900)
+                mask_array = original_image
+                # new_url = api_url+endpoint
+                # request_post= send_backend(lat=lat, lng=lng, zoom=zoom_level, fast_url=new_url)
+                # mask_json = request_post.json()
+                # mask_array = np.array(mask_json['output_mask'])
+                # mask = cv2.normalize(mask_array, dst=None, alpha=0,
+                #                 beta=255,norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+                # mask = smooth_image(mask, 900)
 
                 #calculations
                 sqrm = np.rint(rooftop_area_calculator(zoom=zoom_level, lat=lat, mask=mask_array)).astype(np.int32)
                 #need to add a city grabber to pass through the energy output function, default is tokyo
                 solar_kw = np.rint(solar_panel_energy_output(area=sqrm, location=city_name)).astype(np.int32)
                 co2 = np.rint(co2_calculator(solar_panel_output = solar_kw)["Coal Offset"]).astype(np.int32)
-                car_equiv = str(np.rint(car_equivalent(co2)).astype(np.int32))
-                homes = str(np.rint(home_electricity(solar_kw)).astype(np.int32))
+                car_equiv = np.rint(car_equivalent(co2)).astype(np.int32)
+                homes = np.rint(home_electricity(solar_kw)).astype(np.int32)
                 placeholder = True
 
         with sub_col_2:
@@ -190,7 +191,7 @@ def main():
                     st.components.v1.html(map_html, width=650, height=500)
 
         st.write('')
-        st.write(f'Totals for {neighborhood.capitalize()}, {city_name.capitalize()}')
+        st.write(f'Totals for {location.title()}')
         #area for calculation totals
 
         sub_col_3, sub_col_4, sub_col_5 = st.columns([3,1,3])
@@ -202,29 +203,18 @@ def main():
                     #When placeholder is False then this will display
                     st.write(f"Square meters: 0 m²")
                     st.write("")
-                    st.write(f"Energy: 0 Kw hours")
+                    st.write(f"Energy: 0 megawatts")
                     st.write('')
-                    st.write(f"CO2 offset: 0 metric tons")
+                    st.write(f"CO₂ offset: 0 metric tons")
                 if placeholder:
                     #When placeholder is True this will display, conditional formatting
-                    if len(str(sqrm)) <= 3:
-                        st.write(f"Square meters: {str(sqrm)} m²")
-                    elif len(str(sqrm)) == 4:
-                        st.write(f"Square meters: {str(sqrm)[:1]},{str(sqrm)[1:]} m²")
-                    elif len(str(sqrm)) == 5:
-                        st.write(f"Square meters: {str(sqrm)[:2]},{str(sqrm)[2:]} m²")
-                    elif len(str(sqrm)) == 6:
-                        st.write(f"Square meters: {str(sqrm)[:3]},{str(sqrm)[3:]} m²")
-                    st.write("")
-                    if len(str(solar_kw)) < 10:
-                        st.write(f"Energy: {str(solar_kw)[:-6]} million kWh per year")
-                    elif len(str(solar_kw)) >= 10:
-                        st.write(f"Energy: {str(solar_kw)[0]},{str(solar_kw)[1:4]} million kWh per year")
-                    st.write("")
-                    if str(co2)[0] == "1":
-                        st.write(f"CO2 offset: {str(co2)[:-6]} metric ton")
-                    elif str(co2)[0] != "1":
-                        st.write(f"CO2 offset: {str(co2)[:-6]} metric tons")
+                    sqrm_round = np.round(sqrm, -3).astype(np.int32)
+                    st.write(f"Square meters: {sqrm_round:,} m²")
+                    mega_w = np.round((solar_kw / 1000), -3).astype(np.int32)
+                    st.write(f"Energy: {mega_w:,}  megawatts per year")
+                    co2_metric = np.round((co2 / 1000), -3).astype(np.int32)
+                    st.write(f"CO₂ offset: {co2_metric:,} metric tons")
+
         with sub_col_4:
             with st.container():
                 if not st.session_state.clicked:
@@ -243,17 +233,10 @@ def main():
                 else:
                     st.write("")
                     st.write("")
-                    #st.write("")
-                    if len(homes) <= 3:
-                        st.write(f"This would power {homes} homes for a year!")
-                    elif len(homes) == 4:
-                        st.write(f"This would power {homes[:1]},{homes[1:]} homes for a year!")
-                    elif len(homes) == 5:
-                        st.write(f"This would power {homes[:2]},{homes[2:]} homes for a year!")
-                    if len(car_equiv) <= 3:
-                        st.write(f"That's {car_equiv} cars driving for one year!")
-                    elif len(car_equiv) == 4:
-                        st.write(f"That's {car_equiv[:1]},{car_equiv[1:]} cars driving for one year!")
+                    homes_round = np.round(homes, -3).astype(np.int32)
+                    st.write(f"This would power {homes_round:,} homes for a year!")
+                    car_round = np.round(car_equiv, -3).astype(np.int32)
+                    st.write(f"These are the equivalent emissions produced by {car_round:,} cars a year")
 
     with col2:
         #middle column, just white space to add a more balanced look
@@ -318,7 +301,7 @@ def main():
                 st.image([original_image], use_column_width=True)
             with sub_col_7:
                 st.write("Mask")
-                st.image([mask], use_column_width=True)
+                st.image([mask_array], use_column_width=True)
 
 
 if __name__ == "__main__":
