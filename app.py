45a7a689557482bfe93c968ae9571c6447aee540
Sarah Underwood <sstollunderwood@yahoo.com>
2024-03-07 17:24:52 +0900

    changing layout

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def main():
     #LAYOUT / CONFIG
     #set page configuration to wide
     st.set_page_config(layout="wide",initial_sidebar_state="collapsed")
-    process_start = False
+
     #shrink the page margins
     margins_css = """
         <style>
@@ -65,14 +65,16 @@ def main():
     button_color = st.markdown("""
         <style>
         div.stButton > button:first-child {
-            background-color: rgb(32, 176, 0);
+            background-color: rgb(195, 27, 27);
+            height: 3em;
+            width: 6em;
         }
         </style>""", unsafe_allow_html=True)
 
     hover_color = st.markdown("""
         <style>
         div.stButton > button:hover {
-            background-color: rgb(18, 107, 0);
+            background-color: #ff000050;
         }
         </style>""", unsafe_allow_html=True)
 
@@ -87,6 +89,30 @@ def main():
         </style>
         """, unsafe_allow_html=True)
 
+    st.markdown(
+        """
+        <style>
+            div[data-testid="column"]:nth-of-type(1)
+            {
+                text-align: center;
+            }
+
+        </style>
+        """,unsafe_allow_html=True
+        )
+
+    st.markdown(
+        """
+        <style>
+            div[data-testid="column"]:nth-of-type(3)
+            {
+                text-align: center;
+            }
+
+        </style>
+        """,unsafe_allow_html=True
+        )
+
     # Set up the page title
     st.title("Solar Potential Map")
 
@@ -99,7 +125,7 @@ def main():
 
 
     #Establish column layout, details in left small column, map in larger right column
-    col1, col2, col3= st.columns([4, 0.5, 6.5])
+    col1, col2, col3= st.columns([4, 0.5, 5.5])
 
     with col1:
         st.write('')
@@ -129,32 +155,20 @@ def main():
         st.stop()
 
     with col1:
-        st.markdown(
-            """
-            <style>
-                div[data-testid="column"]:nth-of-type(1)
-                {
-                    text-align: center;
-                }
-
-            </style>
-            """,unsafe_allow_html=True
-            )
         placeholder = False
         sub_col_1, sub_col_2 = st.columns([3,3])
         with sub_col_1:
             if st.button("Calculate!", on_click=click_button):
-                process_start = True
                 #this is where the back end call will go
                 original_image = get_gmaps_image(lat, lng, zoom_level)
-                #mask_array = original_image
-                new_url = api_url+endpoint
-                request_post= send_backend(lat=lat, lng=lng, zoom=zoom_level, fast_url=new_url)
-                mask_json = request_post.json()
-                mask_array = np.array(mask_json['output_mask'])
-                mask = cv2.normalize(mask_array, dst=None, alpha=0,
-                                beta=255,norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
-                mask = smooth_image(mask, 900)
+                mask_array = original_image
+                # new_url = api_url+endpoint
+                # request_post= send_backend(lat=lat, lng=lng, zoom=zoom_level, fast_url=new_url)
+                # mask_json = request_post.json()
+                # mask_array = np.array(mask_json['output_mask'])
+                # mask = cv2.normalize(mask_array, dst=None, alpha=0,
+                #                 beta=255,norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+                # mask = smooth_image(mask, 900)
 
                 #calculations
                 sqrm = np.rint(rooftop_area_calculator(zoom=zoom_level, lat=lat, mask=mask_array)).astype(np.int32)
@@ -163,7 +177,6 @@ def main():
                 co2 = np.rint(co2_calculator(solar_panel_output = solar_kw)["Coal Offset"]).astype(np.int32)
                 car_equiv = str(np.rint(car_equivalent(co2)).astype(np.int32))
                 homes = str(np.rint(home_electricity(solar_kw)).astype(np.int32))
-                process_start = False
                 placeholder = True
         with sub_col_2:
             if st.session_state.clicked:
@@ -176,54 +189,67 @@ def main():
         st.write(f'Totals for {neighborhood.capitalize()}, {city_name.capitalize()}')
         #area for calculation totals
 
-        with st.container(border=True):
-            # html_code = """<div id = 'inner_container'></div>"""
-            # st.markdown(html_code, unsafe_allow_html=True)
-            if not placeholder:
-                #When placeholder is False then this will display
-                st.write(f"Square meters: 0 m²")
-                st.write(f"Solar Kilowatts: 0 Kw hours")
-                st.write(f"Equivalent CO2: 0 metric tons")
-            if placeholder:
-                #When placeholder is True this will display, conditional formatting
-                if len(str(sqrm)) <= 3:
-                    st.write(f"Square meters: {str(sqrm)} m²")
-                elif len(str(sqrm)) == 4:
-                    st.write(f"Square meters: {str(sqrm)[:1]},{str(sqrm)[1:]} m²")
-                elif len(str(sqrm)) == 5:
-                    st.write(f"Square meters: {str(sqrm)[:2]},{str(sqrm)[2:]} m²")
-                elif len(str(sqrm)) == 6:
-                    st.write(f"Square meters: {str(sqrm)[:3]},{str(sqrm)[3:]} m²")
-                if len(str(solar_kw)) < 10:
-                    st.write(f"Solar Kilowatts: {str(solar_kw)[:-6]} million kWh per year")
-                elif len(str(solar_kw)) >= 10:
-                    st.write(f"Solar Kilowatts: {str(solar_kw)[0]},{str(solar_kw)[1:4]} million kWh per year")
-                if len(homes) <= 3:
-                    st.write(f"This would power {homes} homes for a year!")
-                elif len(homes) == 4:
-                    st.write(f"This would power {homes[:1]},{homes[1:]} homes for a year!")
-                elif len(homes) == 5:
-                    st.write(f"This would power {homes[:2]},{homes[2:]} homes for a year!")
-                if str(co2)[0] == "1":
-                    st.write(f"Equivalent CO2: {str(co2)[:-6]} metric ton")
-                elif str(co2)[0] != "1":
-                    st.write(f"Equivalent CO2: {str(co2)[:-6]} metric tons")
-                if len(car_equiv) <= 3:
-                    st.write(f"That's {car_equiv} cars driving for one year!")
-                elif len(car_equiv) == 4:
-                    st.write(f"That's {car_equiv[:1]},{car_equiv[1:]} cars driving for one year!")
-
-
-            ## applying style
-            # container_css="""
-            # <style>
-            #     [data-testid='stVerticalBlock']:has(div#inner_container) {
-            #         background: LightBlue;
-            #     }
-            # </style>
-            # """
-
-            # st.markdown(container_css, unsafe_allow_html=True)
+        sub_col_3, sub_col_4, sub_col_5 = st.columns([3,1,3])
+        with sub_col_3:
+            with st.container():
+                # html_code = """<div id = 'inner_container'></div>"""
+                # st.markdown(html_code, unsafe_allow_html=True)
+                if not placeholder:
+                    #When placeholder is False then this will display
+                    st.write(f"Square meters: 0 m²")
+                    st.write("")
+                    st.write(f"Energy: 0 Kw hours")
+                    st.write('')
+                    st.write(f"CO2 offset: 0 metric tons")
+                if placeholder:
+                    #When placeholder is True this will display, conditional formatting
+                    if len(str(sqrm)) <= 3:
+                        st.write(f"Square meters: {str(sqrm)} m²")
+                    elif len(str(sqrm)) == 4:
+                        st.write(f"Square meters: {str(sqrm)[:1]},{str(sqrm)[1:]} m²")
+                    elif len(str(sqrm)) == 5:
+                        st.write(f"Square meters: {str(sqrm)[:2]},{str(sqrm)[2:]} m²")
+                    elif len(str(sqrm)) == 6:
+                        st.write(f"Square meters: {str(sqrm)[:3]},{str(sqrm)[3:]} m²")
+                    st.write("")
+                    if len(str(solar_kw)) < 10:
+                        st.write(f"Energy: {str(solar_kw)[:-6]} million kWh per year")
+                    elif len(str(solar_kw)) >= 10:
+                        st.write(f"Energy: {str(solar_kw)[0]},{str(solar_kw)[1:4]} million kWh per year")
+                    st.write("")
+                    if str(co2)[0] == "1":
+                        st.write(f"CO2 offset: {str(co2)[:-6]} metric ton")
+                    elif str(co2)[0] != "1":
+                        st.write(f"CO2 offset: {str(co2)[:-6]} metric tons")
+        with sub_col_4:
+            with st.container():
+                if not st.session_state.clicked:
+                    st.write("")
+                else:
+                    st.write("")
+                    st.write("")
+                    #st.write("")
+                    st.image("st_images/red_home.png", width=40)
+                    st.image("st_images/red_car.png", width=40)
+        with sub_col_5:
+            with st.container():
+                if not st.session_state.clicked:
+                    st.write("")
+                else:
+                    st.write("")
+                    st.write("")
+                    #st.write("")
+                    if len(homes) <= 3:
+                        st.write(f"This would power {homes} homes for a year!")
+                    elif len(homes) == 4:
+                        st.write(f"This would power {homes[:1]},{homes[1:]} homes for a year!")
+                    elif len(homes) == 5:
+                        st.write(f"This would power {homes[:2]},{homes[2:]} homes for a year!")
+                    st.write("")
+                    if len(car_equiv) <= 3:
+                        st.write(f"That's {car_equiv} cars driving for one year!")
+                    elif len(car_equiv) == 4:
+                        st.write(f"That's {car_equiv[:1]},{car_equiv[1:]} cars driving for one year!")
 
     with col2:
         #middle column, just white space to add a more balanced look
@@ -266,18 +292,29 @@ def main():
 
     # Display the map:
     with col3:
-        while process_start:
-                st.spinner("Processing image...")
+        st.markdown(
+            """
+            <style>
+                button[title^=Exit]+div [data-testid=stImage]{
+                    text-align: center;
+                    display: block;
+                    margin-left: auto;
+                    margin-right: auto;
+                    width: 100%;
+                }
+            </style>
+            """, unsafe_allow_html=True
+        )
         if not st.session_state.clicked:
             st.components.v1.html(map_html, width=650, height=500)
         else:
-            sub_col_3, sub_col_4 = st.columns([4,4])
-            with sub_col_3:
+            sub_col_6, sub_col_7 = st.columns([4,4])
+            with sub_col_6:
                 st.write("Original")
-                st.image([original_image], width=350)
-            with sub_col_4:
+                st.image([original_image], use_column_width=True)
+            with sub_col_7:
                 st.write("Mask")
-                st.image([mask], width=350)
+                st.image([mask_array], use_column_width=True)
 
 
 if __name__ == "__main__":
